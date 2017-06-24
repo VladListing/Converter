8d7288c11160f05eca9093a7de568bcad7c818dd
VladListing <chvch@mail.ru>
2017-06-24 21:52:17 +0700

    корректировка класса RandoomString

--- ConverterBinaryToCSV/GeneratorBinaryFiles/RandomString.cs
+++ ConverterBinaryToCSV/GeneratorBinaryFiles/RandomString.cs
@@ -2,16 +2,18 @@
 
 namespace GeneratorBinaryFiles
 {
-    //класс 'RandomString' заполняем столбец 'коментарий' случайными значениями
-
+    ///<symmary>
+    ///класс 'RandomString' заполняем столбец 'коментарий' случайными значениями
+    ///<symmary>
+    
     class RandomString
     {
-        Random r = new Random();
+        Random r = new Random(DateTime.Now.Millisecond);
 
-        private string sumString = "";// "склеянная строка со случайными значениями по текущей сделке"
-        private int number = 0; //номер варианта случайной строки
-        private int profit = 0; //прибыль
-        private int loss = 0;   //убыток
+        private string sumString = null;// "склеянная строка со случайными значениями по текущей сделке"
+        private int number = 0;         //номер варианта случайной строки
+        private int profit = 0;         //сумма по прибыльной сделки
+        private int loss = 0;           //сумма по убыточной сделке
         public string GetCommentRandom()
         {
             number = r.Next(0, 6);
@@ -20,12 +22,24 @@ namespace GeneratorBinaryFiles
                         
             switch (number)
             {
-                case 0: sumString = "   trade:Sell   result:Profit     " + "   +" + profit + " $"; break;
-                case 1: sumString = "   trade:Sell   result:Loss           " + loss + " $"; break;
-                case 2: sumString = "   trade:Bay    result:Profit     " + "  +" + profit + " $"; break;
-                case 3: sumString = "   trade:Bay    result:Loss          " + loss + " $"; break;
-                case 4: sumString = "   trade:Bay    result:Stoploss  " + loss + " $"; break;
-                case 5: sumString = "   trade:Sell   result:Stoploss  " + loss + " $"; break;
+                case 0:
+                    sumString = "   trade:Sell   result:Profit     " + "   +" + profit + " $";
+                    break;
+                case 1:
+                    sumString = "   trade:Sell   result:Loss           " + loss + " $";
+                    break;
+                case 2:
+                    sumString = "   trade:Bay    result:Profit     " + "  +" + profit + " $";
+                    break;
+                case 3:
+                    sumString = "   trade:Bay    result:Loss          " + loss + " $";
+                    break;
+                case 4:
+                    sumString = "   trade:Bay    result:Stoploss  " + loss + " $";
+                    break;
+                case 5:
+                    sumString = "   trade:Sell   result:Stoploss  " + loss + " $";
+                    break;
             }
 
             return sumString;
