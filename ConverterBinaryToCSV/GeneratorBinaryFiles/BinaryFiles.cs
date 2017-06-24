9d171220ee3e0b5936e6d1fd1356a7a21bc60cdc
VladListing <chvch@mail.ru>
2017-06-24 23:14:41 +0700

    кректировка класса BinaryFiles

--- ConverterBinaryToCSV/GeneratorBinaryFiles/BinaryFiles.cs
+++ ConverterBinaryToCSV/GeneratorBinaryFiles/BinaryFiles.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 namespace GeneratorBinaryFiles
 ///<symmary>
-/// класс 'BinaryFiles' генерирует бинарные файлы с заданым количеством строк.
+/// класс 'BinaryFiles' генерирует бинарный файл с заданым количеством строк, заданой структурой.
 ///<symmary>
 {
     class BinaryFiles
@@ -28,30 +28,32 @@ namespace GeneratorBinaryFiles
             }
         }
         
-        private string pathBinaryFiles = ""; //путь и имя создаваемого бинарного файла.
-        private long quantityLine = 0;       //количество строк в создаваемом бинарном файле.
-        private long counter = 0;            //счетчик записаных в файл строк.        
+        private string _pathBinaryFiles = ""; //путь и имя создаваемого бинарного файла.
+        private long _quantityLine = 0;       //количество строк в создаваемом бинарном файле.
+        private long _counter = 0;            //счетчик записаных в файл строк.        
                 
         public void BinaryFilesGener(string pathBinaryFiles, long quantityLine )
         {
-            this.pathBinaryFiles = pathBinaryFiles;
-            this.quantityLine = quantityLine;
+            this._pathBinaryFiles = pathBinaryFiles;
+            this._quantityLine = quantityLine;
 
             Console.SetWindowSize(90, 20);
             
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
+            Console.WriteLine("\n\n");
             Console.WriteLine("Ждите, выполняется запись в  файл: {0} ", pathBinaryFiles);
-            Console.WriteLine("");
+            Console.WriteLine();
 
             RandomString randoomString = new RandomString();
             
-        try
+          try
           {
+                //todo: тут иемеет смысл передать только один раз в конструктор quantityLine
+                //ProcessMappingBase processMapping = new ProcessGenerationMapping(quantityLine);
+
                 ProcessMapping processMapping = new ProcessMapping();
 
-                File.WriteAllText(pathBinaryFiles, "");//очистка содержимого файла.
+                //очистка содержимого файла (на случай перезаписи существующего файла).
+                File.WriteAllText(pathBinaryFiles, "");
                 
                 using (BinaryWriter writer = new BinaryWriter(File.Open(pathBinaryFiles, FileMode.Append, FileAccess.Write)))
                                  
@@ -65,17 +67,16 @@ namespace GeneratorBinaryFiles
                     writer.Write(trades.volume);
                     writer.Write(trades.comment);
 
-                    counter++;                     
+                    _counter++;                     
                 }
                 //отображение текушего процента выполнения    
-                processMapping.processMappingInPercent(this.quantityLine, pathBinaryFiles, 1, false);                              
+                processMapping.processMappingInPercent(this._quantityLine, pathBinaryFiles, 1, false);                              
             }
 
             Console.Write("\r");
             Console.Write("выполнено: 100 % ");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("в файл {0} записано  {1}  строк(и) ", pathBinaryFiles, counter);
+            Console.WriteLine("\n");
+            Console.WriteLine("в файл {0} записано  {1}  строк(и) ", pathBinaryFiles, _counter);
                       
           }            
           catch (Exception m)
